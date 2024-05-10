82bdaa43fe92eec69a150a0c74a3a4dc0e5e1541
bonniekiprop <bonniekiprop12@gmail.com>
2024-05-10 13:21:27 +0300

    adding crud operation

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,13 @@
+//establishing connection to database
 const mongoose = require('mongoose');
 
 const connectDb = async () => {
   try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log("Database connected", 
+    connect.connection.host,
+    connect.connection.name
+);
   }catch (err) {
     console.log(err);
     process.exit(1);
